2c93bfbb5ab7851ffc4442e02651250c92961bf5
allytotheson <82621261+allytotheson@users.noreply.github.com>
2023-11-04 19:05:07 -0400

    Update clock.py

--- clock.py
+++ clock.py
@@ -2,16 +2,44 @@ from cmu_graphics import *
 
 
 def onAppStart(app):
-    app.width = 1920
-    app.height = 1080
+    app.width = 1400
+    app.height = 800
     app.gx = 0
     app.gy = (2/3)*app.height
 
+    app.sy = app.gy
+    print(app.sy)
+    
+
+    app.studyLength = 0.25
+    app.totalSeconds = app.studyLength * 60
+
+
+    app.moonR = 30
+    app.cx = app.moonR
+    app.cy = app.gy - app.moonR
+    app.goingUp = True
 
 def onKeyPress(app, key):
     pass
 def redrawAll(app):
     drawRect(app.gx, app.gy, app.width, app.height, fill = "darkSeaGreen")
+    drawRect(0, 0, app.width, app.sy, fill = "lightBlue")
+    drawCircle(app.cx, app.cy, app.moonR, fill = "silver")
+def onStep(app):
+    app.stepsPerSecond = 10
+
+
+    app.cx += app.width/(app.totalSeconds*app.stepsPerSecond)
+
+    if app.cy <= 30:
+        app.goingUp = not app.goingUp
+
+    if app.goingUp:
+        app.cy -= app.sy/(0.5*app.totalSeconds*app.stepsPerSecond)
+    else:
+        app.cy += app.sy/(0.5*app.totalSeconds*app.stepsPerSecond)
+
 
 def main():
     runApp()
