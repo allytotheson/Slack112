ae32f976fcbe4ed30c33a6d69216ce91d4ac411c
allytotheson <82621261+allytotheson@users.noreply.github.com>
2023-11-04 17:17:41 -0400

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,1 +1,17 @@
 from cmu_graphics import *
+
+
+def onAppStart(app):
+    app.gx = 0
+    app.gy = (2/3)*app.height
+
+
+def onKeyPress(app, key):
+    pass
+def redrawAll(app):
+    drawRect(app.gx, app.gy, app.width, app.height, fill = "darkSeaGreen")
+
+def main():
+    runApp()
+
+main()
