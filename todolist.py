75d5d73e3c89760635d7112d935311548e47cc24
allytotheson <82621261+allytotheson@users.noreply.github.com>
2023-11-04 23:27:03 -0400

    Update todolist.py

--- todolist.py
+++ todolist.py
@@ -9,11 +9,11 @@ def noteVars(app):
     app.i = len(app.toDoList)
     app.note = ""
     app.noteFill = {0:(None, False)}
-    app.x, app.y = app.width/4, app.height/4
-    app.dx, app.dy = app.width/2, app.height/2
+    app.x, app.y = app.width*(3/4), app.height/4
+    app.dx, app.dy = app.width/8, app.height/2
 
     app.boxDim = 20
-    app.bx, app.by = app.x + 20, app.y + app.dy/6
+    app.bx, app.by = app.x + 10, app.y + app.dy/6
     app.listDim = [(app.bx, app.by, app.bx + app.boxDim,
                     app.by + app.boxDim)]
     
@@ -21,19 +21,20 @@ def noteVars(app):
 def drawNote(app):
     drawRect(app.x, app.y, app.dx, app.dy, fill = "cornSilk",
              border = "gray")
-    
-    drawLabel("To-Do List", app.x+app.dx/2, app.y + 20, size = 18)
-    drawLabel("Start typing to add to list!", app.x+app.dx/2, app.y + 40)
+    drawLabel("To-Do List", app.x+app.dx/2, app.y + 20, size = 18, font = "monospace", bold = True)
+    drawLabel("Start typing to add a task!", app.x+app.dx/2, app.y + 40, size = 10, font = "monospace")
 
     #drawing the blank box
     drawRect(app.bx, app.by + (50*app.i), app.boxDim, app.boxDim, fill = app.noteFill[app.i][0], border = "black")
-    drawLabel(app.note, app.bx+app.boxDim+10,  app.by + (50*app.i) + app.boxDim/2, align = "left")
+    drawLabel(app.note, app.bx+app.boxDim+10,  app.by + (50*app.i) + app.boxDim/2, font = "monospace"
+              ,align = "left")
 
 
     for i in range(len(app.toDoList)):
         drawRect(app.bx, app.by + (50*i), app.boxDim, app.boxDim,
                 fill = app.noteFill[i][0], border = "black")
-        drawLabel(app.toDoList[i], app.bx+app.boxDim+10,  app.by + (50*i) + app.boxDim/2, align = "left")
+        drawLabel(app.toDoList[i], app.bx+app.boxDim+10,  app.by + (50*i) + app.boxDim/2, 
+                  font = "monospace", align = "left")
 
 def redrawAll(app):
     drawNote(app)
@@ -49,15 +50,20 @@ def onMousePress(app, mouseX, mouseY):
     for i in range(len(app.listDim)):
         x0, y0, x1, y1 = app.listDim[i]
         if x0 <= mouseX <= x1 and y0 <= mouseY <= y1:
-            app.noteFill[i] = ("lightGreen", True)
+            if app.noteFill[i][-1] == True:
+                app.noteFill[i] = (None, False)
+            else:
+                app.noteFill[i] = ("seaGreen", True)
+        
 
 def onMouseMove(app, mouseX, mouseY):
     for i in range(len(app.listDim)):
         x0, y0, x1, y1 = app.listDim[i]
-        if x0 <= mouseX <= x1 and y0 <= mouseY <= y1:
+        if x0 <= mouseX <= x1 and y0 <= mouseY <= y1 and app.noteFill[i][-1] == False:
             app.noteFill[i] = ("lightGreen", False)
         else:
-            app.noteFill[i] = (None, False)
+            if app.noteFill[i][-1] == False:
+                app.noteFill[i] = (None, False)
 def onKeyPress(app, key):
     if len(key) == 1:
         app.note += key
@@ -65,13 +71,13 @@ def onKeyPress(app, key):
         app.note += " "
     elif key == "backspace":
         app.note = app.note[:-1]
-    elif key == "enter":
+    elif key == "enter" and len(app.toDoList) < 6:
         app.toDoList.append(app.note)
         app.note = ""
         app.i = len(app.toDoList)
         app.listDim.append((app.bx, app.by + (50*app.i), app.bx + app.boxDim,
                             app.by + (50*app.i) + app.boxDim))
-        app.noteFill[app.i] = None
+        app.noteFill[app.i] = (None, False)
 
 def main():
     runApp()
